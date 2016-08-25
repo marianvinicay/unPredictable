991911e39940135d815d599c1b45f95c1c2fc054
Majo <Majo@mikes-imac.local>
2016-08-25 11:30:07 +0200

    Initial Commit

---
+++ macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  (un)Predictable
+//
+//  Created by Majo on 25/08/16.
+//  Copyright © 2016 MarVin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
