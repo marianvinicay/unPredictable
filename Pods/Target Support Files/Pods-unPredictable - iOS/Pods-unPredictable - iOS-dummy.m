ac1625b14783f26f5ed001ab7167de99f2d8b929
marvin <mv@marvin99.com>
2018-01-09 22:04:01 +0100

    idk

---
+++ Pods/Target Support Files/Pods-unPredictable - iOS/Pods-unPredictable - iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_unPredictable___iOS : NSObject
+@end
+@implementation PodsDummy_Pods_unPredictable___iOS
+@end
---
+++ Pods/Target Support Files/Pods-unPredictable - macOS/Pods-unPredictable - macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_unPredictable___macOS : NSObject
+@end
+@implementation PodsDummy_Pods_unPredictable___macOS
+@end
